9a6a52626ff68ab21d9bf9643a59a24c387ca372
Nimira43 <n.rabone72@aol.com>
2024-12-24 13:36:56 +0000

    Generate frequency values and inverse of frequency

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 population_size = 2.3e5
-print(population_size)
+print('Population Size: ', population_size)
+
+# 1 / f
+f = np.linspace(0, 5, 18)
+1/f
+
+print('Frequency value (f): ', f)
+print('Inverse of frequency values (1/f)', 1/f)

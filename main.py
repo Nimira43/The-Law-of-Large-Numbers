970f74baf955a91d835b27ebc04002730a5452ae
Nimira43 <n.rabone72@aol.com>
2024-12-24 15:10:47 +0000

    Generate a logarithmic scale of numbers, then taking the inverse of these values and plotting them while skipping some data points to make the plot more manageable.

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Initialise population size
-population_size = 230  # Reduced for better performance in visualisation
+population_size = 2.3e5
 print('Population Size: ', population_size)
 
-# Generate frequency values (f)
-frequency_values = np.logspace(np.log10(0.001), np.log10(10), population_size)
-print('Frequency values (f): ', frequency_values)
+# Generate Population of Numbers
+population = 1 / np.logspace(np.log10(0.001), np.log10(10), int(population_size))
+print('Population of Numbers: ', population)
 
-# Calculate the inverse of frequency values (1/f)
-inverse_f = 1 / frequency_values
-print('Inverse of frequency values (1/f): ', inverse_f)
-
-# Plot the inverse frequency values
-plt.figure(figsize=(10, 6))
-plt.plot(inverse_f, label='1/f')
-plt.title('Inverse of Frequency Values')
-plt.xlabel('Index')
-plt.ylabel('1/f')
-plt.grid(True)
-plt.legend()
+# Plot the Inverse Values with Skipping
+skip = int(1e3)
+plt.plot(population[::skip])
 plt.show()
 

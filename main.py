bdbe53542c7e78a53bbfb2cb7d07b5ce9f79823f
Nimira43 <n.rabone72@aol.com>
2024-12-24 13:42:46 +0000

    Included handling of division by zero

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Initialise population size
 population_size = 2.3e5
 print('Population Size: ', population_size)
 
-# 1 / f
-f = np.linspace(0, 5, 18)
-1/f
-
+# Generate frequency value (f) and handle division by zero
+epsilon = 1e-10 # small value to avoid division by 0
+f = np.linspace(0, 5, 18) + epsilon
 print('Frequency value (f): ', f)
-print('Inverse of frequency values (1/f)', 1/f)
+
+inverse_f = 1 / f
+print('Inverse of frequency values (1/f)', inverse_f)

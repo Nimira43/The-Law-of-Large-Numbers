4d77bf927e82cd1ab2e084c9c1243b6b586d6d2c
Nimira43 <n.rabone72@aol.com>
2024-12-24 14:28:15 +0000

    Using logspace to generate values on a logarithmic scale

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ population_size = 2.3e5
 print('Population Size: ', population_size)
 
 # Generate frequency value (f)
-f = np.linspace(0.001, 5, 18)
+f = np.logspace(np.log10(0.001), np.log10(5), 18)
 print('Frequency value (f): ', f)
 
 inverse_f = 1 / f

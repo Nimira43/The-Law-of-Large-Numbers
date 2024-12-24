d8e0caa3e10abeea2d45dcbe72df074927691df7
Nimira43 <n.rabone72@aol.com>
2024-12-24 18:23:01 +0000

    Tidying the outputted code

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Population Size
 population_size = 2.3e5
-print('Population Size: ', population_size)
 
 # Sampling Parameters
 sample_size = 50
@@ -11,5 +11,23 @@ number_of_samples = 500
 # Generate Population of Numbers
 population = 1 / np.logspace(np.log10(0.001), np.log10(10), int(population_size))
 
-print('Random Sample of 50 from Population:', np.random.choice(population, size=sample_size))
-print('Mean of Another Random Sample of 50: ', np.mean(np.random.choice(population, size=sample_size)))
+# True mean
+true_mean = np.mean(population)
+
+# Output 
+print('[-----------------------------------------------]')
+print('[*] Population Size: ', population_size)
+print('[-----------------------------------------------]')
+print('[*] Generated Population Size: ', population_size)
+print('[-----------------------------------------------]')
+print('[*] True Mean of Population: ', true_mean)
+print('[*] Sample Size: ', sample_size)
+print('[*] Number of Sample: ', number_of_samples)
+print('[-----------------------------------------------]')
+print
+
+# Random Sampling
+
+print('[*] Random Sample of 50 from Population:', np.random.choice(population, size=sample_size))
+print('[*] Mean of Another Random Sample of 50: ', np.mean(np.random.choice(population, size=sample_size)))
+print('[-----------------------------------------------]')
